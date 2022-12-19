af05471ccde7bd996f915f608ee7aa750ad63ccd
Sebastianidm <sebastiandiazmiranda@gmail.com>
2022-12-19 20:22:42 -0300

    se realizan ajustes a la version desktop

--- src/assets/components/Home.jsx
+++ src/assets/components/Home.jsx
@@ -1,29 +1,39 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div className='containerHome'>
-        <div className='containerHijo'>
-        <h1>Make Remote Work</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, id similique atque tempora accusantium doloribus corporis perspiciatis provident magni, ea obcaecati! Adipisci sint placeat laborum ipsum nihil ab quaerat hic.
-        Itaque quam atque numquam, voluptatibus, labore reprehenderit deserunt officia asperiores accusamus nesciunt ad libero soluta sunt consequatur harum quaerat assumenda nostrum molestias dolores. Cumque modi nihil facilis, alias reprehenderit blanditiis.</p>
+    <div className="containerHome">
+      <div className="ladoIzquierdo">
+        <h1 className="titulo">
+          Make <br /> Remote Work
+        </h1>
+        <p>
+          Get your team in sync, no matter your location.
+          <br />
+          Streamline processes, create team rituals, and
+          <br />
+          watch productivity soar.
+        </p>
 
-        <button className='botonHome'>Learn More</button>
-        <ul className='listaIcono'>
-            <li>icono</li>
-            <li>icono</li>
-            <li>icono</li>
-            <li>icono</li>
+        <button className="botonHome">Learn More</button>
+
+        <ul className="listaIcono">
+          <li>icono</li>
+          <li>icono</li>
+          <li>icono</li>
+          <li>icono</li>
         </ul>
+      </div>
 
-
-        </div>
-
-        <div>
-        <img  className='imgHome' src="/src/assets/img/image-hero-desktop.png" alt="imagenMain" />
-        </div>
+      <div>
+        <img
+          className="imgHome"
+          src="/src/assets/img/image-hero-desktop.png"
+          alt="imagenMain"
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
